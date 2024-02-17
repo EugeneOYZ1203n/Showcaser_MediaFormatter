abfb7f6436f9f2a15d4d418376514fc4ff424f72
Eugene Oh Yun Zheng <eugeneoyz.12.03.n@gmail.com>
2024-02-18 00:59:33 +0800

    Added basic UI for JobListItem Accordion

--- src/SubComponents/JobListItem/JobListItem.tsx
+++ src/SubComponents/JobListItem/JobListItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './JobListItem.css'
 
 function JobListItem({title, subjobs}:{
@@ -8,9 +9,29 @@ function JobListItem({title, subjobs}:{
   }>
 }) {
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const overallProgress = (subjobs.reduce((acc,el)=>acc+el.progress, 0) / subjobs.length).toFixed(1)
+  const status = overallProgress==="0.0"?"In Queue":overallProgress==="100.0"?"Completed":"In Progress";
+
   return (
     <>
-       <div>JobListItem</div>
+      <div className='JobListItem'>
+        <button className={'JobListItem__AccordionButton'} onClick={()=>setIsOpen(!isOpen)}>
+          <span className='JobListItem__Title'>{title}</span>
+          <span className='JobListItem__Status'>{status}</span>
+          <span className='JobListItem__Progress'>{`${overallProgress}%`}</span>
+          <span>{isOpen?"-":"+"}</span>
+        </button>
+        <div className='JobListItem__SubJobList'>
+          {subjobs.map((subjob, index)=>
+            (<div className={isOpen?'SubJobListItem show':'SubJobListItem'} key={index}>
+              <span className='SubJobListItem__Title'>{subjob.title}</span>
+              <span className='SubJobListItem__Progress'>{`${subjob.progress.toFixed(1)}%`}</span>
+            </div>)
+          )}
+        </div>
+      </div>
     </>
   )
 }
