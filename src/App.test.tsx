ed7f959ad64bc2a0702409562a982e24ee600753
Eugene Oh Yun Zheng <eugeneoyz.12.03.n@gmail.com>
2024-02-17 09:48:14 +0800

    Removing unneeded files and imports

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest'
-import '@testing-library/jest-dom'
+
 import App from './App.tsx'
 
 describe('App', () => {
