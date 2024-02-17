ab6cf1eb15b09f773045c7594cad281842534f79
Eugene Oh Yun Zheng <eugeneoyz.12.03.n@gmail.com>
2024-02-18 00:48:31 +0800

    Update JobListItem.test.tsx
    - Using Query Selector and Get By Text for referencing elements as it feels more intuitive
    - Removed tests that might have to be changed if design style changed

--- src/SubComponents/JobListItem/JobListItem.test.tsx
+++ src/SubComponents/JobListItem/JobListItem.test.tsx
@@ -1,80 +1,76 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
 import JobListItem from './JobListItem';
 
 describe('JobListItem SubComponent', () => {
-    render(<JobListItem title='Test' subjobs={[
-        {title: "Sub Test 1", progress: 0},
-        {title: "Sub Test 2", progress: 80},
-        {title: "Sub Test 3", progress: 100}
-    ]}/>);
+    //Checking job information displayed correctly
 
-    const accordionButton = screen.getByTestId("Accordion_Button");
-
-    it('Title is Visible', () => {
+    it('Displays correct title', () => {
+        render(<JobListItem title='Test' subjobs={[]}/>);
         expect(screen.getByText(/^Test$/)).toBeVisible();
     });
 
-    it('Creates clickable Accordion button', () => {
-        expect(accordionButton).toBeInTheDocument();
-        expect(accordionButton).not.toBeDisabled();
-    });
-
-    const subJobItem1 = screen.getByTestId("Accordion_List").children[0];
-    const subJobItem2 = screen.getByTestId("Accordion_List").children[1];
-    const subJobItem3 = screen.getByTestId("Accordion_List").children[2];
-
-    it('Accordion has as many children as sub jobs', () => {
-        expect(subJobItem1).toBeInTheDocument();
-        expect(subJobItem2).toBeInTheDocument();
-        expect(subJobItem3).toBeInTheDocument();
-    });
-
-    it('Before clicking, Accordion is not visible', () => {
-        expect(subJobItem1).not.toBeVisible();
-        expect(subJobItem2).not.toBeVisible();
-        expect(subJobItem3).not.toBeVisible();
-    });
-
-    fireEvent.click(accordionButton)
-
-    it('After clicking, Accordion is now visible', () => {
-        expect(subJobItem1).toBeVisible();
-        expect(subJobItem2).toBeVisible();
-        expect(subJobItem3).toBeVisible();
-    });
-
-    it('Accordion displays titles of sub jobs', () => {
-        expect(subJobItem1).toHaveTextContent("Sub Test 1");
-        expect(subJobItem2).toHaveTextContent("Sub Test 2");
-        expect(subJobItem3).toHaveTextContent("Sub Test 3");
-    });
-
-    it('Accordion displays progress of sub jobs', () => {
-        expect(subJobItem1).toHaveTextContent("0%");
-        expect(subJobItem2).toHaveTextContent("80%");
-        expect(subJobItem3).toHaveTextContent("100%");
-    });
-
-    it('Sub jobs have correct classes based on progress', () => {
-        expect(subJobItem1.classList.contains('InQueue'))
-        expect(subJobItem2.classList.contains('InProgress'))
-        expect(subJobItem3.classList.contains('Completed'))
-    });
-
     it('Displays correct overall progress', () => {
-        expect(accordionButton).toHaveTextContent("60%");
+        const {container:container1} = render(
+        <JobListItem title='Test' subjobs={[
+            {title: "Sub Test 1", progress: 0},
+            {title: "Sub Test 2", progress: 80},
+            {title: "Sub Test 3", progress: 100}
+        ]}/>);
+        expect(container1.querySelector(".JobListItem__Progress"))
+        .toHaveTextContent("60.0%");
+
+        const {container:container2} = render(
+        <JobListItem title='Test' subjobs={[
+            {title: "Sub Test 1", progress: 0},
+            {title: "Sub Test 2", progress: 30},
+            {title: "Sub Test 3", progress: 50}
+        ]}/>);
+        expect(container2.querySelector(".JobListItem__Progress"))
+        .toHaveTextContent("26.7%");
     });
 
     it('Displays correct status', () => {
-        expect(accordionButton).toHaveTextContent("In Progress");
+        const {container:container1} = render(
+        <JobListItem title='Test' subjobs={[
+            {title: "Sub Test 1", progress: 0},
+            {title: "Sub Test 2", progress: 0}
+        ]}/>);
+        expect(container1.querySelector(".JobListItem__Status"))
+        .toHaveTextContent("In Queue");
+
+        const {container:container2} = render(
+        <JobListItem title='Test' subjobs={[
+            {title: "Sub Test 1", progress: 20},
+            {title: "Sub Test 2", progress: 80}
+        ]}/>);
+        expect(container2.querySelector(".JobListItem__Status"))
+        .toHaveTextContent("In Progress");
+
+        const {container:container3} = render(
+        <JobListItem title='Test' subjobs={[
+            {title: "Sub Test 1", progress: 100},
+            {title: "Sub Test 2", progress: 100}
+        ]}/>);
+        expect(container3.querySelector(".JobListItem__Status"))
+        .toHaveTextContent("Completed");
     });
 
-    fireEvent.click(accordionButton)
+    it('Has as many children as sub jobs', () => {
+        const {container:container1} = render(
+        <JobListItem title='Test' subjobs={[
+            {title: "Sub Test 1", progress: 0},
+            {title: "Sub Test 2", progress: 0}
+        ]}/>);
+        expect(container1.querySelector(".JobListItem__SubJobList")?.children.length).toBe(2)
 
-    it('After clicking again, Accordion is no longer visible', () => {
-        expect(subJobItem1).not.toBeVisible();
-        expect(subJobItem2).not.toBeVisible();
-        expect(subJobItem3).not.toBeVisible();
+        const {container:container2} = render(
+        <JobListItem title='Test' subjobs={[
+            {title: "Sub Test 1", progress: 0},
+            {title: "Sub Test 2", progress: 0},
+            {title: "Sub Test 3", progress: 0},
+            {title: "Sub Test 4", progress: 0}
+        ]}/>);
+        expect(container2.querySelector(".JobListItem__SubJobList")?.children.length).toBe(4)
     });
 });
