54ef4c9cdad33172706880adde0275dc2a985bf0
Eugene Oh Yun Zheng <eugeneoyz.12.03.n@gmail.com>
2024-02-18 12:37:43 +0800

    Added Color changing for different statuses and progress bar that scales with progress
    -Edited Jobs Page dummy inputs to view css changes more easily
    - Colors are changed by styling the main-color css variable through javascript
    - Progress bar width and color are also done by styling in javascript
    - Added hover effects for sub jobs
    - Added pointer cursor for the button

--- src/Components/JobsPage/JobsPage.tsx
+++ src/Components/JobsPage/JobsPage.tsx
@@ -8,13 +8,18 @@ function JobsPage() {
             <h1>Current Session Jobs</h1>
             <JobListItem title='Test' subjobs={[
               {title: "Sub Test 1", progress: 0},
-              {title: "Sub Test 2", progress: 80},
+              {title: "Sub Test 2", progress: 0},
+              {title: "Sub Test 3", progress: 0}
+            ]}/>
+            <JobListItem title='Test' subjobs={[
+              {title: "Sub Test 1", progress: Math.round(Math.random()*30+5)},
+              {title: "Sub Test 2", progress: Math.round(Math.random()*30+45)},
               {title: "Sub Test 3", progress: 100}
             ]}/>
             <h1>Past Session Jobs</h1>
             <JobListItem title='Test' subjobs={[
-              {title: "Sub Test 1", progress: 0},
-              {title: "Sub Test 2", progress: 80},
+              {title: "Sub Test 1", progress: 100},
+              {title: "Sub Test 2", progress: 100},
               {title: "Sub Test 3", progress: 100}
             ]}/>
        </div>
--- src/SubComponents/JobListItem/JobListItem.tsx
+++ src/SubComponents/JobListItem/JobListItem.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { CSSProperties, useState } from 'react'
 import './JobListItem.css'
 
 function JobListItem({title, subjobs}:{
@@ -14,10 +14,26 @@ function JobListItem({title, subjobs}:{
   const overallProgress = (subjobs.reduce((acc,el)=>acc+el.progress, 0) / subjobs.length).toFixed(1)
   const status = overallProgress==="0.0"?"In Queue":overallProgress==="100.0"?"Completed":"In Progress";
 
+  const getColorStyle = (_progress: number) => (
+    _progress==0?
+      {"--main-color":"var(--secondary-color)"} as CSSProperties:
+    _progress==100?
+      {"--main-color":"color-mix(in srgb, var(--secondary-color) 60%, var(--background-color))"} as CSSProperties:
+      {"--main-color":"var(--tertiary-color)"} as CSSProperties)
+  
+  const getProgressStyle = (_progress: number) => (
+    {
+      "width": `${(_progress*0.98+2).toFixed(0)}%`,
+      "background-color": `color-mix(in srgb, var(--main-color) ${(_progress*0.4+20).toFixed(0)}%, var(--background-color))`,
+    } as CSSProperties
+  )
+  
+
   return (
     <>
-      <div className='JobListItem'>
+      <div className='JobListItem' style={getColorStyle(Number(overallProgress))}>
         <button className={'JobListItem__AccordionButton'} onClick={()=>setIsOpen(!isOpen)}>
+          <div className='JobListItem__ProgressBar' style={getProgressStyle(Number(overallProgress))}></div>
           <span className='JobListItem__Title'>{title}</span>
           <span className='JobListItem__Status'>{status}</span>
           <span className='JobListItem__Progress'>{`${overallProgress}%`}</span>
@@ -25,12 +41,16 @@ function JobListItem({title, subjobs}:{
         </button>
         <div className='JobListItem__SubJobList'>
           {subjobs.map((subjob, index)=>
-            (<div className={isOpen?'SubJobListItem show':'SubJobListItem'} key={index}>
+            (<div className={isOpen?'SubJobListItem show':'SubJobListItem'} 
+            style={getColorStyle(subjob.progress)}
+            key={index}>
+              <div className='SubJobListItem__ProgressBar' style={getProgressStyle(subjob.progress)}></div>
               <span className='SubJobListItem__Title'>{subjob.title}</span>
               <span className='SubJobListItem__Progress'>{`${subjob.progress.toFixed(1)}%`}</span>
             </div>)
           )}
         </div>
+        
       </div>
     </>
   )
