113c32d805842dcead60e85a59ac5a6aee10314d
Eugene Oh Yun Zheng <eugeneoyz.12.03.n@gmail.com>
2024-02-19 16:33:52 +0800

    Add Dropzone functionality
    - Upload box takes a onDrop function which it will use, passing accepted files as params whenever a new onDrop event occurs
    - permanentActive can be set to True and it will not hide itself when no files are dragged on top of it
    - acceptedTypes allow specific types to be allowed only
    - Added css styling for it

--- src/SubComponents/Preview/Preview.tsx
+++ src/SubComponents/Preview/Preview.tsx
@@ -1,11 +1,18 @@
-import UploadBox from '../UploadBox/UploadBox'
-import './Preview.css'
+import { useCallback } from "react";
+import UploadBox from "../UploadBox/UploadBox";
+import "./Preview.css";
 
 function Preview() {
+  const onDrop = useCallback((acceptedFiles: File[]) =>{
+    console.log(acceptedFiles.map(file=>file.path))
+  }, [])
+  
   return (
     <>
-       <div>Preview</div>
-       <UploadBox />
+      <div className="Preview">
+        Preview
+        <UploadBox onDropFunction={onDrop}/>
+      </div>
     </>
   );
 }
--- src/SubComponents/UploadBox/UploadBox.tsx
+++ src/SubComponents/UploadBox/UploadBox.tsx
@@ -1,11 +1,47 @@
-import "./UploadBox.css";
+import './UploadBox.css';
+import { Accept, useDropzone} from 'react-dropzone';
+import { useCallback } from 'react';
+/* 
+Features: 
+1. Given a onDropFunction to pass the files information to (name and path??)
+2. Given a storage location to save files on local filesystem?? Maybe just a context will do
+3. Given a list of file formats to accept
+4. Creates a dropzone for submitting files (Can handle multiple uploads)
+*/
 
-function UploadBox() {
+const defaultAcceptedTypes = {
+  'image/png': ['.png'],
+  "image/gif": ['.gif'],
+  "image/jpeg": ['.jpg', '.jpeg'],
+  "video/mp4": ['.mp4']
+}  
+
+function UploadBox({onDropFunction, permanentActive = false, acceptedTypes = defaultAcceptedTypes} : 
+  {
+    onDropFunction: (acceptedFiles: File[]) => void,
+    permanentActive?:Boolean,
+    acceptedTypes?: Accept
+  }
+) {
+  
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({
+    onDrop: onDropFunction,
+    noClick: true,
+    accept: acceptedTypes
+  })
+  
   return (
     <>
-      <div>UploadBox</div>
+      <div {...getRootProps({
+        className: `UploadBox ${isDragActive || permanentActive?"Active":""}`
+      })}>
+        <input {...getInputProps()} />
+        <p className='UploadBox__DropzoneText'>
+          {isDragActive || permanentActive?"Drop files here...":""}
+        </p>
+      </div>
     </>
-  );
+  )
 }
 
-export default UploadBox;
+export default UploadBox
