92e6677af34d4ee212235f2ff63799f53f3484ad
JustChocomint <mohzele4@gmail.com>
2024-02-19 19:20:09 +0800

    Created sidebar
    - Togglable sidebar
    - CSS animation
    - useState to track current page
    
    Need to do:
    - Shift useState to App.tsx
    - Actual navigation

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,11 +1,25 @@
 import './Sidebar.css'
+import './Navigation.css'
+import MenuIcon from './MenuIcon'
+import NavigationBubble from './Bubble'
+import { useState }from 'react';
 
 function Sidebar() {
+  const [toggle, setToggle] = useState(false);
+  const [currentPage, setCurrentPage] = useState('Clipping');
+  // Pages: Clipping, Formatting
+  const menuClick =():void =>{
+      setToggle(!toggle);
+      console.log("Herer");
+  }
   return (
     <>
-       <div className='Sidebar highlightBorder'>
-        Sidebar
-        </div>
+      <div className='Sidebar highlightBorder'>
+      <MenuIcon toggle={toggle} menuClick={menuClick} />
+      <NavigationBubble toggle={toggle} currentPage={currentPage} setCurrentPage={setCurrentPage} page='Clipping'/>
+      <NavigationBubble toggle={toggle} currentPage={currentPage} setCurrentPage={setCurrentPage} page='Format'/>
+      <NavigationBubble toggle={toggle} currentPage={currentPage} setCurrentPage={setCurrentPage} page='Jobs'/>
+      </div>
     </>
   )
 }
